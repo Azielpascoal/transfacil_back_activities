3611b9201e9a7be01912bdccca1dd77cfe446f18
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2021-11-16 03:28:36 +0100

    alter database name

--- env.php
+++ env.php
@@ -5,4 +5,4 @@
    putenv('TRANSFACIL_DB_MYSQL_DBNAME=transfacil');
    putenv('TRANSFACIL_DB_MYSQL_USER=root');
    putenv('TRANSFACIL_DB_MYSQL_PASSWORD=');
-   putenv('TRANSFACIL_DB_MYSQL_PORT=3306');
+   putenv('TRANSFACIL_DB_MYSQL_PORT=3306'); 
